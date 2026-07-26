32a06065ea2ce0780e6054a06119d7d500a906f9
hastagnitin <nitin978032@gmail.com>
2026-07-26 15:21:43 +0530

    refactor: update alerting methods to use environment variables for Slack and Gmail

--- drift_engine/core.py
+++ drift_engine/core.py
@@ -2,47 +2,35 @@ import boto3
 import requests
 import json
 import os
+import smtplib
+from email.mime.text import MIMEText
+
 INSTANCE_ID = 'i-057f05151b227214d'
 EXPECTED_TYPE = 't3.micro'
-WEBHOOK_URL = 'SLACK_WEBHOOK_URL'
+
+WEBHOOK_URL = os.getenv('SLACK_WEBHOOK_URL')
+GMAIL_USER = os.getenv('GMAIL_USER')
+GMAIL_PASS = os.getenv('GMAIL_PASS')
 
 def send_slack_alert(message):
     slack_data = {"text": f"🚨 *DriftWatch Alert* 🚨\n{message}"}
-    requests.post(
-        WEBHOOK_URL, 
-        data=json.dumps(slack_data),
-        headers={'Content-Type': 'application/json'}
-    )
+    if WEBHOOK_URL:
+        requests.post(WEBHOOK_URL, data=json.dumps(slack_data))
 
-def check_drift():
-    print("DriftWatch Engine Started")
-    print("------------------------------------------------")
-    print(f"Expected State: Instance ID -> {INSTANCE_ID}, Type -> {EXPECTED_TYPE}")
-    print("Fetching actual details from AWS...")
+def send_email_alert(message):
+    if not GMAIL_USER or not GMAIL_PASS:
+        print("Gmail credentials are not set!")
+        return
+        
+    msg = MIMEText(message)
+    msg['Subject'] = '🚨 DriftWatch Alert: AWS EC2 Deviation Detected'
+    msg['From'] = GMAIL_USER
+    msg['To'] = GMAIL_USER
     
     try:
-        ec2 = boto3.client('ec2', region_name='ap-south-1')
-        response = ec2.describe_instances(InstanceIds=[INSTANCE_ID])
-        actual_type = response['Reservations'][0]['Instances'][0]['InstanceType']
-        
-        print(f"Actual State: Type -> {actual_type}")
-        print("------------------------------------------------")
-        
-        if actual_type == EXPECTED_TYPE:
-            print("No drift detected. Infrastructure is in sync.")
-            print("------------------------------------------------")
-        else:
-            print("DRIFT DETECTED: Manual change identified.")
-            print(f"Expected Type: {EXPECTED_TYPE}")
-            print(f"Actual Type:   {actual_type}")
-            print("------------------------------------------------")
-            
-            alert_msg = f"Drift Detected on Instance `{INSTANCE_ID}`!\n*Expected Type:* `{EXPECTED_TYPE}`\n*Actual Type:* `{actual_type}`"
-            send_slack_alert(alert_msg)
-            
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+            server.login(GMAIL_USER, GMAIL_PASS)
+            server.send_message(msg)
+            print("Gmail alert sent successfully!")
     except Exception as e:
-        print(f"Error: {str(e)}")
-        send_slack_alert(f"AWS Check Failed: {str(e)}")
-
-if __name__ == "__main__":
-    check_drift()
+        print(f"Error sending email: {e}")
