041f9db261a27fe3dc8549e50941c4dc51f7a043
hastagnitin <nitin978032@gmail.com>
2026-07-27 15:04:41 +0530

    update AWS instance for drift testing with dynamic naming

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,12 @@ provider "aws" {
   region = "ap-south-1" 
 }
 
-resource "aws_instance" "drift_test" {
-  ami           = "ami-09d88f7c4c272b0c5" 
+resource "aws_instance" "web_servers" {
+  count         = 3
+  ami           = "ami-0c2af51e265bd5e0e"
   instance_type = "t3.micro"
-
+  
   tags = {
-    Name = "DriftWatch-Sandbox"
+    Name = "DriftWatch-Test-${count.index + 1}"
   }
 }
