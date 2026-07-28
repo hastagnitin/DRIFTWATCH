a57278b940c97df3f711a2ef7eaf6e620c8879fd
hastagnitin <nitin978032@gmail.com>
2026-07-28 11:46:23 +0530

    add security group resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,27 @@ resource "aws_s3_bucket" "drift_test_bucket" {
      Environment = "Dev"
   }
 }
+
+resource "aws_security_group" "drift_test_sg" {
+  name        = "driftwatch-test-sg"
+  description = "Security group for DriftWatch testing"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "DriftWatch-SG-Test"
+    Environment = "Dev"
+  }
+}
