5236236e163c7bd9a41400d222903ebf7bf9e18b
hastagnitin <nitin978032@gmail.com>
2026-08-01 21:04:50 +0530

    add region specification

--- start_all.sh
+++ start_all.sh
@@ -1,11 +1,13 @@
-EC2_STOPPED=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped" --query "Reservations[*].Instances[*].InstanceId" --output text)
+REGION="ap-south-1"
+
+EC2_STOPPED=$(aws ec2 describe-instances --region $REGION --filters "Name=instance-state-name,Values=stopped" --query "Reservations[*].Instances[*].InstanceId" --output text)
 
 if [ -n "$EC2_STOPPED" ]; then
-    aws ec2 start-instances --instance-ids $EC2_STOPPED
+    aws ec2 start-instances --region $REGION --instance-ids $EC2_STOPPED
 fi
 
-RDS_STOPPED=$(aws rds describe-db-instances --query "DBInstances[?DBInstanceStatus=='stopped'].DBInstanceIdentifier" --output text)
+RDS_STOPPED=$(aws rds describe-db-instances --region $REGION --query "DBInstances[?DBInstanceStatus=='stopped'].DBInstanceIdentifier" --output text)
 
 for db in $RDS_STOPPED; do
-    aws rds start-db-instance --db-instance-identifier $db
+    aws rds start-db-instance --region $REGION --db-instance-identifier $db
 done
--- stop_all.sh
+++ stop_all.sh
@@ -1,11 +1,13 @@
-EC2_RUNNING=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text)
+REGION="ap-south-1"
+
+EC2_RUNNING=$(aws ec2 describe-instances --region $REGION --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text)
 
 if [ -n "$EC2_RUNNING" ]; then
-    aws ec2 stop-instances --instance-ids $EC2_RUNNING
+    aws ec2 stop-instances --region $REGION --instance-ids $EC2_RUNNING
 fi
 
-RDS_AVAILABLE=$(aws rds describe-db-instances --query "DBInstances[?DBInstanceStatus=='available'].DBInstanceIdentifier" --output text)
+RDS_AVAILABLE=$(aws rds describe-db-instances --region $REGION --query "DBInstances[?DBInstanceStatus=='available'].DBInstanceIdentifier" --output text)
 
 for db in $RDS_AVAILABLE; do
-    aws rds stop-db-instance --db-instance-identifier $db
+    aws rds stop-db-instance --region $REGION --db-instance-identifier $db
 done
