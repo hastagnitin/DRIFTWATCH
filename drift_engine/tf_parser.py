393812799fb2c5fef08b960709c75b166b93e4b6
hastagnitin <nitin978032@gmail.com>
2026-08-16 17:45:41 +0530

    Refactor Terraform state loading

--- drift_engine/tf_parser.py
+++ drift_engine/tf_parser.py
@@ -7,16 +7,36 @@ def load_terraform_state(state_path: str) -> dict:
     except FileNotFoundError:
         print(f"Error: Terraform state file not found at '{state_path}'")
         return {}
+    except json.JSONDecodeError:
+        print(f"Error: Terraform state file at '{state_path}' is corrupted or invalid JSON.")
+        return {}
         
     resources = {}
+    
     for resource in state.get("resources", []):
         r_type = resource["type"]
         
-        if r_type in ["archive_file", "aws_iam_role_policy_attachment"]:
+        if r_type == "archive_file":
             continue
             
         for instance in resource.get("instances", []):
             attrs = instance.get("attributes", {})
+            
+            if r_type == "aws_iam_role_policy_attachment":
+                role_name = attrs.get("role")
+                policy_arn = attrs.get("policy_arn")
+                if role_name and policy_arn:
+                    if role_name not in resources:
+                        resources[role_name] = {
+                            "type": "aws_iam_role", 
+                            "name": role_name, 
+                            "attributes": {"id": role_name, "name": role_name, "attached_policies": []}
+                        }
+                    if "attached_policies" not in resources[role_name]["attributes"]:
+                        resources[role_name]["attributes"]["attached_policies"] = []
+                    resources[role_name]["attributes"]["attached_policies"].append(policy_arn)
+                continue
+
             resource_id = attrs.get("id")
             
             tags = attrs.get("tags", {})
@@ -28,13 +48,20 @@ def load_terraform_state(state_path: str) -> dict:
                 name = attrs.get("identifier", "Unknown")
             elif r_type == "aws_iam_role":
                 name = attrs.get("name", "Unknown")
+                if "attached_policies" not in attrs:
+                    attrs["attached_policies"] = []
             else:
                 name = "Unknown"
             
             if resource_id:
+                if resource_id in resources and r_type == "aws_iam_role":
+                    existing_policies = resources[resource_id]["attributes"].get("attached_policies", [])
+                    attrs["attached_policies"] = list(set(attrs.get("attached_policies", []) + existing_policies))
+                    
                 resources[resource_id] = {
                     "type": r_type, 
                     "name": name, 
                     "attributes": attrs
                 }
+                
     return resources
